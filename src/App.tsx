9102b7cb1753e567a7ec873bc846e3fd54d66332
Anamarija <anamarijamileska@outlook.com>
2026-05-08 03:46:37 +0200

    feat(pages and reouting): add FieldDetailsPage composing all sub-components and register /fields/:id route in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import DiseaseRiskPage from './pages/DiseaseRiskPage';
-import FertilizerPage from './pages/FertilizerPage';
-import LoginPage from './pages/LoginPage';
-import FieldsPage from './pages/FieldsPage';
-import AddFieldPage from './pages/AddFieldPage';
-import CropAnalysisPage from './pages/CropAnalysisPage';
-import ReportsPage from './pages/ReportsPage';
-import WeatherPage from './pages/WeatherPage';
-import type React from 'react';
-import { AuthProvider } from './providers/AuthProvider';
-import ProtectedRoute from './components/ProtectedRoute';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import DiseaseRiskPage from "./pages/DiseaseRiskPage";
+import FertilizerPage from "./pages/FertilizerPage";
+import LoginPage from "./pages/LoginPage";
+import FieldsPage from "./pages/FieldsPage";
+import AddFieldPage from "./pages/AddFieldPage";
+import CropAnalysisPage from "./pages/CropAnalysisPage";
+import ReportsPage from "./pages/ReportsPage";
+import WeatherPage from "./pages/WeatherPage";
+import type React from "react";
+import { AuthProvider } from "./providers/AuthProvider";
+import ProtectedRoute from "./components/ProtectedRoute";
+import FieldDetailsPage from "./pages/FieldDetailsPage";
 
 export default function App(): React.ReactElement {
   return (
@@ -21,33 +22,69 @@ export default function App(): React.ReactElement {
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<LoginPage />} />
           // Protected routes
-          <Route 
+          <Route
             path="/crop-analysis"
-            element={<ProtectedRoute><CropAnalysisPage /></ProtectedRoute>}
+            element={
+              <ProtectedRoute>
+                <CropAnalysisPage />
+              </ProtectedRoute>
+            }
           />
           <Route
             path="/disease-risk"
-            element={<ProtectedRoute><DiseaseRiskPage /></ProtectedRoute>}
+            element={
+              <ProtectedRoute>
+                <DiseaseRiskPage />
+              </ProtectedRoute>
+            }
           />
           <Route
             path="/fertilizer"
-            element={<ProtectedRoute><FertilizerPage /></ProtectedRoute>}
+            element={
+              <ProtectedRoute>
+                <FertilizerPage />
+              </ProtectedRoute>
+            }
           />
           <Route
             path="/reports"
-            element={<ProtectedRoute><ReportsPage /></ProtectedRoute>}
+            element={
+              <ProtectedRoute>
+                <ReportsPage />
+              </ProtectedRoute>
+            }
           />
           <Route
             path="/weather"
-            element={<ProtectedRoute><WeatherPage /></ProtectedRoute>}
+            element={
+              <ProtectedRoute>
+                <WeatherPage />
+              </ProtectedRoute>
+            }
           />
           <Route
             path="/fields"
-            element={<ProtectedRoute><FieldsPage /></ProtectedRoute>}
+            element={
+              <ProtectedRoute>
+                <FieldsPage />
+              </ProtectedRoute>
+            }
           />
           <Route
             path="/fields/new"
-            element={<ProtectedRoute><AddFieldPage /></ProtectedRoute>}
+            element={
+              <ProtectedRoute>
+                <AddFieldPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/fields/:id"
+            element={
+              <ProtectedRoute>
+                <FieldDetailsPage />
+              </ProtectedRoute>
+            }
           />
         </Routes>
       </AuthProvider>
